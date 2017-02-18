fea0b8040b7c9656745ceaca4543e8b869ef8002
kiyotaka-nakashima <kiyotaka.nakashima@dena.jp>
2017-02-18 15:31:57 +0900

    ジェネレターの設定

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,12 @@ module WebNitaku
     config.i18n.load_path +=
       Dir[Rails.root.join('config', 'locales', '**', '*.{rb, yml}'.to_s)]
     config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controller false
+      g.view_specs false
+    end
   end
 end
