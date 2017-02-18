635dd4ca09a25e019d6282d1729fb50ee6a93298
rasayama <nanor610@gmail.com>
2017-02-18 17:36:47 +0900

    masterを取り入れるため途中コミット

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+      params[:user].permit(:name, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :users
   get 'signup'  => 'users#new'
 end
